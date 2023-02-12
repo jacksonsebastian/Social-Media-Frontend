4d9c9bfc0d243023f1ce39a42726a6635067e002
Jackson <technologyj4ck@gmail.com>
2023-02-12 20:06:57 +0530

    started implementing social media frontend

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>Profile</div>
+        <div className='homeSide'>Post</div>
+        <div className='rightSide'>Right</div>
+    </div>
+  )
+}
+
+export default Home
